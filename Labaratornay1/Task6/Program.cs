8997084304dc9bfceaa610cce58047eb6471ab0d
KukushkinaUl <ulyanakukushkina@gmail.com>
2022-09-17 09:43:59 +0400

    new

---
+++ Labaratornay1/Task6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стороны треугольника через enter. ");
+            double a, b, c;
+            
+            if ((double.TryParse(Console.ReadLine(), out a)) && (double.TryParse(Console.ReadLine(), out b)) && (double.TryParse(Console.ReadLine(), out c)))
+            {
+                if ((a < b + c) && (b < a + c) && (c < b + a))
+                {
+                    double S, p = 0;
+                    p = (a + b + c) / 2;
+
+                    S = (p * (p - a) * (p - b) * (p - c));
+                    double Otvet = Math.Sqrt(S);
+                    Console.WriteLine("Площадь треугольника: {0}", Otvet);
+                }
+                else { Console.WriteLine("Треугольник не существует."); }
+            }
+            else { Console.WriteLine("Неверно введены данные."); }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Labaratornay1/Task7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите два числа через enter. ");
+            byte num1, num2;
+            int otvetAND, otvetOR, otvetXOR;
+            if ((byte.TryParse(Console.ReadLine(), out num1)) && (byte.TryParse(Console.ReadLine(), out num2)))
+            {
+
+                otvetAND = num1 & num2;
+                Console.WriteLine("Побитовое И: {0}", otvetAND);
+                otvetOR = num1 | num2;
+                Console.WriteLine("Побитовое ИЛИ: {0}", otvetOR);
+                otvetXOR = num1 ^ num2;
+                Console.WriteLine("Побитовое исключающие ИЛИ: {0}", otvetXOR);
+            }
+            else { Console.WriteLine("Неверно введены данные."); }
+            Console.ReadLine();
+
+        }
+    }
+}
